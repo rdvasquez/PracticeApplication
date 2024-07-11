eda4e82b8b910ba5e8a334331cb6543db90e1aa8
ROWENA VASQUEZ <vasquezrd@gmail.com>
2024-07-11 10:26:29 +0100

    fixed

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+const person = {
+  name: "Bob",
+  age: 30,
+  favouriteColour: "red",
+  sayHello: function (name) {
+    console.log("Hello " + name + "!");
+  },
+};
+person.sayHello("Alice");
