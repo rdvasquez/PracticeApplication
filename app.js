22cb61748959b0be57459d203ab194b9d704a597
ROWENA VASQUEZ <vasquezrd@gmail.com>
2024-07-12 16:54:22 +0100

    index

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-console.log("Hello World!");
+console.log("Welcome to my Gallery!");
+console.log(document);
 
-const person = {
-  name: "Bob",
-  age: 30,
-  favouriteColour: "red",
-  sayHello: function (name) {
-    console.log("Hello " + name + "!");
-  },
-};
-person.sayHello("Alice");
+let currentIndex = 0;
+
+const largeImage = document.getElementById("large-image");
+const images = ["PH eagle.webp", "amore1.webp", "amore2.webp"];
+
+const thumbnail1 = document.getElementById("thumbnail1");
+thumbnail1.addEventListener("mouseover", function () {
+  largeImage.src = images[0];
+});
+
+const thumbnail2 = document.getElementById("thumbnail2");
+thumbnail2.addEventListener("mouseover", function () {
+  thumbnail2;
+  largeImage.src = images[1];
+});
+
+const thumbnail3 = document.getElementById("thumbnail3");
+thumbnail3.addEventListener("mouseover", function () {
+  thumbnail3;
+  largeImage.src = images[2];
+});
+
+function nextBtn() {
+  currentIndex++;
+  largeImage.src = images[currentIndex];
+}
+document.getElementById("button-next").onclick = () => nextBtn();
+
+function prevBtn() {
+  currentIndex--;
+  largeImage.src = images[currentIndex];
+}
+document.getElementById("button-previous").onclick = () => prevBtn();
